c1989e17419c3906bbcbff80486520d5472ee09f
jahandideh-iman <jahandideh_iman@yahoo.com>
2013-10-15 12:54:12 +0330

    a template to work on

---
+++ Classes/JG_Bonus_Base.cpp
@@ -0,0 +1,11 @@
+#include "JG_Bonus_Base.h"
+
+
+JG_Bonus_Base::JG_Bonus_Base(void)
+{
+}
+
+
+JG_Bonus_Base::~JG_Bonus_Base(void)
+{
+}
---
+++ Classes/JG_Bonus_Base.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "c:\cocos2d-x-2.2.0\cocos2dx\sprite_nodes\ccsprite.h"
+class JG_Bonus_Base :
+	public CCSprite
+{
+public:
+	JG_Bonus_Base(void);
+	~JG_Bonus_Base(void);
+};
+

bbc739b6394ce9206564918328dad539cb254558
jahandideh-iman <jahandideh_iman@yahoo.com>
2013-10-17 12:09:13 +0330

    fix new system

---
+++ Classes/JG_Ball.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "c:\cocos2d-x-2.2.0\cocos2dx\sprite_nodes\ccsprite.h"
+
+using namespace cocos2d;
+class JG_Ball :
+	public CCSprite
+{
+public:
+	JG_Ball(void);
+	~JG_Ball(void);
+};
+
